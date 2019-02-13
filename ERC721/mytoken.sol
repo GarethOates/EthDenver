b049befdcce5c2b3700b93aa9b9cb49bc2a4a8fa
Gareth Oates <goatie@gmail.com>
2019-02-13 12:29:21 -0700

    Updated token

--- ERC721/mytoken.sol
+++ ERC721/mytoken.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.5.0;
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract MyToken is ERC721 {
+    address owner;
     uint total_pokemons = 0;
+
     mapping(uint => address) id_to_owner;
     mapping(uint => Pokemon) pokemon_list;
-    address owner;
 
     struct Pokemon {
+        uint id;
         uint attack;
         uint health;
         string image_url;
@@ -18,6 +20,7 @@ contract MyToken is ERC721 {
         owner = msg.sender;
 
         Pokemon memory instance = Pokemon(total_pokemons, 100, 100, "example.com");
+        pokemon_list[total_pokemons] = instance;
 
         _mint(owner, total_pokemons);
         id_to_owner[0] = owner;
@@ -30,5 +33,25 @@ contract MyToken is ERC721 {
         safeTransferFrom(to, id_to_owner[tokenId], tokenId);
 
         id_to_owner[tokenId] = to;
+    }
+
+    function createPokemon(uint _attack, uint _health, string memory _image_url) public payable {
+        require(_attack < 100, "Attack too strong");
+        require(_health > 0, "Cannot have negative health");
+        require(msg.value >= 0.005 ether, "Insufficient Funds");
+
+        Pokemon memory instance = Pokemon(total_pokemons, _attack, _health, _image_url);
+        pokemon_list[total_pokemons] = instance;
+
+        _mint(msg.sender, total_pokemons);
+        id_to_owner[total_pokemons] = msg.sender;
+
+        total_pokemons++;
+    }
+
+    function cashOut(address payable _payTo) public {
+        require(msg.sender == owner, "You are not the contract owner");
+
+        _payTo.transfer(address(this).balance);
     }
 }
